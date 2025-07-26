ed53867b0b0cf1c476fb257a6771de4e1225f729
Daniel Pitts <git@stochastictinker.com>
2025-07-26 08:57:22 -0700

    Prepare next snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.stochastictinkr.lwjgl"
-version = "1.0.1"
+version = "1.0.2-SNAPSHOT"
 
 repositories {
     mavenCentral()
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.21"
-    id("com.stochastictinkr.lwjgl") version "1.0.1"
+    id("com.stochastictinkr.lwjgl") version "1.0.2-SNAPSHOT"
     application
 }
 
